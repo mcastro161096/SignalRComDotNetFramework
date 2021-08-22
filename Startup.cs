14a768086fdacbd22fe962e52c3e008b4203526f
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-22 19:43:14 -0300

    Adicionando Startup class.

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalRComDotNetFramework.Startup))]
+
+namespace SignalRComDotNetFramework
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
