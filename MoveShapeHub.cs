ebf47e76edf7cb1c09b97cdab708a4f26ec5235f
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-22 19:59:22 -0300

    Adicionada a classe do SignalR que fará o update nos clients.

---
+++ MoveShapeHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace SignalRComDotNetFramework
+{
+    public class MoveShapeHub : Hub
+    {
+        public void UpdateModel(ShapeModel clientModel)
+        {
+            clientModel.LastUpdateBy = Context.ConnectionId;
+            Clients.AllExcept(clientModel.LastUpdateBy).UpdateShape(clientModel);
+        }
+
+        public class ShapeModel
+        {
+            [JsonProperty("left")]
+            public double Left { get; set; }
+            [JsonProperty("top")]
+            public double Top { get; set; }
+            public string LastUpdateBy { get; set; }
+        }
+
+    }
+}
